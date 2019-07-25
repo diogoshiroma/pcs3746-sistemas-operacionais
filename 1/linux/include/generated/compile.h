d1698260857d0704742c1716164014493f6bfd1f
diogo <diogoshiroma@hotmail.com>
2019-07-25 11:14:35 -0300

    fase 3

--- 1/linux/include/generated/compile.h
+++ 1/linux/include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 79 */
+/* This file is auto generated, version 90 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#79 Wed Jun 26 22:50:54 UTC 2019"
+#define UTS_VERSION "#90 Fri Jun 28 14:46:38 UTC 2019"
 #define LINUX_COMPILE_BY "student"
-#define LINUX_COMPILE_HOST "26edae6488b2"
+#define LINUX_COMPILE_HOST "bc7d2e034ce1"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.9) "
